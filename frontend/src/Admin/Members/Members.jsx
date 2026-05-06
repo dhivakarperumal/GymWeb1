f45f7fb0bf276274a27a0d1bd1995bb77e637484
qtechx001 <qtechx001@gmail.com>
2026-05-06 17:24:12 +0530

    Button double Issue Fix

--- frontend/src/Admin/Members/Members.jsx
+++ frontend/src/Admin/Members/Members.jsx
@@ -349,7 +349,7 @@ const Members = () => {
 
         {/* ➕ ADD MEMBER + IMPORT/EXPORT */}
         <div className="flex flex-wrap items-center gap-3 w-full sm:w-auto">
-          <button
+          {/* <button
             onClick={downloadTemplate}
             className="flex items-center justify-center gap-2 px-5 py-2 rounded-lg font-semibold text-white
             bg-white/5 border border-white/10 hover:bg-white/10 transition-all flex-1 sm:flex-none"
@@ -357,32 +357,32 @@ const Members = () => {
           >
             <Download size={16} className="text-blue-500" />
             Template
-          </button>
+          </button> */}
 
-          <button
+          {/* <button
             onClick={() => document.getElementById("importExcel").click()}
             className="flex items-center justify-center gap-2 px-5 py-2 rounded-lg font-semibold text-white
             bg-white/5 border border-white/10 hover:bg-white/10 transition-all flex-1 sm:flex-none"
           >
             <Import size={16} className="text-emerald-500" />
             Import
-          </button>
-          <input
+          </button> */}
+          {/* <input
             type="file"
             id="importExcel"
             className="hidden"
             accept=".xlsx, .xls"
             onChange={handleImport}
-          />
+          /> */}
 
-          <button
+          {/* <button
             onClick={exportToExcel}
             className="flex items-center justify-center gap-2 px-5 py-2 rounded-lg font-semibold text-white
             bg-white/5 border border-white/10 hover:bg-white/20 transition-all flex-1 sm:flex-none"
           >
             <Download size={16} className="text-purple-500" />
             Export
-          </button>
+          </button> */}
 
           <button
             onClick={() => navigate("/admin/pt-form")}
