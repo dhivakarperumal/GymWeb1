82ef1201d402b15667bccea959a5b88d2aadd01f
Dhivakar <dhivakarp305@gmail.com>
2026-06-05 15:23:25 +0530

    fix it

--- frontend/src/Admin/PTForm/PTFormPrint.jsx
+++ frontend/src/Admin/PTForm/PTFormPrint.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate, useSearchParams } from 'react-router-dom';
 import api from '../../api';
 import dayjs from 'dayjs';
 import { Printer, ChevronLeft } from 'lucide-react';
